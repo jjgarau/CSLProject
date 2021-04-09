Moving average and correct env reset
Moving average and correct env reset

models/logger.py
@@ -21,7 +21,6 @@ class Logger:
 
         self.config = config
         self.epoch_df = self._set_up_results_log()
-        self.seed_episode_dfs = {}
         self.current_episode_df, self.current_seed, self.current_policy = None, None, None
 
         self.epoch_returns = []
@@ -46,8 +45,8 @@ class Logger:
             self.config.rolling, min_periods=1).mean()
         self.current_episode_df['Smooth jerk joints'] = self.current_episode_df['Jerk joints'].rolling(
             self.config.rolling, min_periods=1).mean()
-        self.seed_episode_dfs[self.current_seed] = self.current_episode_df
-        self.current_episode_df.to_csv(os.path.join(self.seed_dir, str(self.current_seed) + '.csv'), index=False)
+        self.current_episode_df.to_csv(os.path.join(self.seed_dir,
+                                                    self.current_policy + str(self.current_seed) + '.csv'), index=False)
         self.save_experiment()
         if self.config.plot:
             self.plot_episode_df()
@@ -102,7 +101,8 @@ class Logger:
             file.write(message)
 
     def save_model(self, model, epoch):
-        torch.save(model.state_dict(), os.path.join(self.model_dir, 'model_' + str(epoch) + '.pt'))
+        torch.save(model.state_dict(), os.path.join(self.model_dir, 'model_' + self.current_policy + '_' + str(epoch)
+                                                    + '.pt'))
 
     def _set_up_results_log(self):
 
@@ -127,7 +127,7 @@ class Logger:
     def plot_episode_df(self, figsize=(20, 8)):
         ax = sns.lineplot(x='Episode', y='Smooth return', data=self.current_episode_df)
         ax.grid(color='#c7c7c7', linestyle='--', linewidth=1)
-        path = os.path.join(self.seed_dir, 'return_' + str(self.current_seed) + '.pdf')
+        path = os.path.join(self.seed_dir, 'return_' + self.current_policy + str(self.current_seed) + '.pdf')
         plt.savefig(path, bbox_inches='tight')
         plt.close()
 
@@ -139,12 +139,12 @@ class Logger:
         sns.lineplot(x='Episode', y='Smooth jerk joints', data=self.current_episode_df, ax=axes[1])
         axes[1].grid(color='#c7c7c7', linestyle='--', linewidth=1)
 
-        path = os.path.join(self.seed_dir, 'jerk_' + str(self.current_seed) + '.pdf')
+        path = os.path.join(self.seed_dir, 'jerk_' + self.current_policy + str(self.current_seed) + '.pdf')
         fig.savefig(path, bbox_inches='tight')
         plt.close()
 
     def plot_epoch_df(self, figsize=(20, 8)):
-        ax = sns.lineplot(x='Epoch', y='Mean return', data=self.epoch_df, ci=self.config.ci)
+        ax = sns.lineplot(x='Epoch', y='Mean return', data=self.epoch_df, ci=self.config.ci, hue='Policy')
         ax.grid(color='#c7c7c7', linestyle='--', linewidth=1)
         path = os.path.join(self.dir_name, 'epoch_returns.pdf')
         plt.savefig(path, bbox_inches='tight')
@@ -152,10 +152,10 @@ class Logger:
 
         fig, axes = plt.subplots(1, 2, figsize=figsize)
 
-        sns.lineplot(x='Epoch', y='Mean jerk body', data=self.epoch_df, ci=self.config.ci, ax=axes[0])
+        sns.lineplot(x='Epoch', y='Mean jerk body', data=self.epoch_df, ci=self.config.ci, ax=axes[0], hue='Policy')
         axes[0].grid(color='#c7c7c7', linestyle='--', linewidth=1)
 
-        sns.lineplot(x='Epoch', y='Mean jerk joints', data=self.epoch_df, ci=self.config.ci, ax=axes[1])
+        sns.lineplot(x='Epoch', y='Mean jerk joints', data=self.epoch_df, ci=self.config.ci, ax=axes[1], hue='Policy')
         axes[1].grid(color='#c7c7c7', linestyle='--', linewidth=1)
 
         path = os.path.join(self.dir_name, 'epoch_jerks.pdf')

models/policies.py
@@ -37,6 +37,9 @@ class Policy:
     def load_model(self, model_path):
         raise NotImplementedError
 
+    def new_episode(self):
+        pass
+
 
 class BaselinePolicy(Policy):
 
@@ -75,40 +78,26 @@ class BaselinePolicy(Policy):
         self.ac.eval()
 
 
-class PreviousActionPolicy(Policy):
+class MovingAveragePolicy(BaselinePolicy):
 
-    def __init__(self):
-        pass
+    def __init__(self, observation_space, action_space, window_size=10, hidden_sizes=(64, 64), activation=nn.Tanh):
+        super().__init__(observation_space, action_space, hidden_sizes, activation)
+        self.window_size = window_size
+        self.action_log = []
 
     def get_name(self):
-        return "Previous action"
-
-    def pi(self, obs, act):
-        pass
-
-    def v(self, obs):
-        pass
-
-    def pi_params(self):
-        pass
-
-    def v_params(self):
-        pass
+        return "Moving average"
 
     def step(self, obs):
-        pass
+        a, v, logp = self.ac.step(obs)
+        self.action_log.append(a)
+        if len(self.action_log) > self.window_size:
+            self.action_log = self.action_log[-self.window_size:]
+        a_act = np.mean(self.action_log, axis=0)
+        return (a, a_act), v, logp
 
-    def get_pi(self):
-        pass
-
-    def get_v(self):
-        pass
-
-    def get_model(self):
-        pass
-
-    def load_model(self, model_path):
-        pass
+    def new_episode(self):
+        self.action_log = []
 
 
 class RecurrentPolicy(Policy):
@@ -147,13 +136,13 @@ class RecurrentPolicy(Policy):
         pass
 
 
-class RollingAveragePolicy(Policy):
+class PreviousActionPolicy(Policy):
 
     def __init__(self):
         pass
 
     def get_name(self):
-        return "Rolling average"
+        return "Previous action"
 
     def pi(self, obs, act):
         pass

models/ppo/ppo.py
@@ -56,7 +56,7 @@ def ppo_train(env, policy, seed=0, steps_per_epoch=4000, epochs=50, gamma=0.99, 
 
     # Prepare logger for run
     logger.set_up_seed_episode_df(policy, seed)
-    logger.log(f'Prepare run with seed {seed}')
+    logger.log(f'Prepare run with policy {policy.get_name()} and seed {seed}')
 
     # Random seed
     torch.manual_seed(seed)
@@ -137,6 +137,7 @@ def ppo_train(env, policy, seed=0, steps_per_epoch=4000, epochs=50, gamma=0.99, 
     # Prepare for interaction with environment
     start_time = time.time()
     o, ep_ret, ep_len = env.reset(), 0, 0
+    policy.new_episode()
 
     for epoch in tqdm(range(epochs), desc="Epoch progress"):
 
@@ -146,12 +147,17 @@ def ppo_train(env, policy, seed=0, steps_per_epoch=4000, epochs=50, gamma=0.99, 
 
             a, v, logp = policy.step(torch.as_tensor(o, dtype=torch.float32))
 
-            next_o, r, d, _ = env.step(a)
+            if type(a) is list or type(a) is tuple:
+                a_train, a_act = a
+            else:
+                a_train, a_act = a, a
+
+            next_o, r, d, _ = env.step(a_act)
             ep_ret += r
             ep_len += 1
 
             # save and log
-            buf.store(o, a, r, v, logp)
+            buf.store(o, a_train, r, v, logp)
             # TODO: Logger store v
 
             # Update obs (critical!)
@@ -178,6 +184,7 @@ def ppo_train(env, policy, seed=0, steps_per_epoch=4000, epochs=50, gamma=0.99, 
 
                 buf.finish_path(v)
                 o, ep_ret, ep_len = env.reset(), 0, 0
+                policy.new_episode()
 
         # Save model
         if (epoch % save_freq == 0) or (epoch == epochs - 1):
@@ -206,12 +213,16 @@ def ppo_eval(env, model_path, policy, seed=0, steps_per_epoch=4000, epochs=50, m
 
     start_time = time.time()
     o, ep_ret, ep_len = env.reset(), 0, 0
+    policy.new_episode()
 
     for epoch in tqdm(range(epochs), desc="Epoch progress"):
 
         for t in range(local_steps_per_epoch):
 
             a, v, logp = policy.step(torch.as_tensor(o, dtype=torch.float32))
+
+            if type(a) is list or type(a) is tuple:
+                a = a[1]
 
             next_o, r, d, _ = env.step(a)
             ep_ret += r
@@ -232,3 +243,4 @@ def ppo_eval(env, model_path, policy, seed=0, steps_per_epoch=4000, epochs=50, m
                     print(f'Warning: trajectory cut off by epoch at {ep_len} steps')
 
                 o, ep_ret, ep_len = env.reset(), 0, 0
+                policy.new_episode()